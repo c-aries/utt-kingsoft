74a4ecd534d177e06b549001398ab31b9de7ff59
c-aries <babyaries2@gmail.com>
2011-12-23 02:22:41 +0800

    remove article.c comment

--- src/article.c
+++ src/article.c
@@ -28,15 +28,6 @@ main (int argc, char *argv[])
     g_error ("Can't get %s from %s", "window1", UIFILE);
   }
 
-/*   window = gtk_window_new (GTK_WINDOW_TOPLEVEL); */
-/*   gtk_widget_set_size_request (window, 481, 273); */
-/*   gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER); */
-/*   gtk_window_set_title (GTK_WINDOW (window), "artical typing training"); */
-/*   g_signal_connect (window, "destroy", gtk_main_quit, NULL); */
-
-/*   article = utt_article_new (); */
-/*   gtk_container_add (GTK_CONTAINER (window), article); */
-
   gtk_builder_connect_signals (builder, NULL);
   g_object_unref (G_OBJECT (builder));
 
