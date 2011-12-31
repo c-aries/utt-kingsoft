12a02b0766f4b46814413b8d5de49a307cc85fae
c-aries <babyaries2@gmail.com>
2012-01-01 02:45:18 +0800

    filechooser

--- src/article.c
+++ src/article.c
@@ -9,11 +9,25 @@
 
 #define UIFILE PKGDATADIR "/article-ui.xml"
 
+static void
+on_open_item_activate (GtkMenuItem *menuitem, gpointer user_data)
+{
+  GtkWidget *chooser = user_data;
+  gchar *filename;
+
+  if (gtk_dialog_run (GTK_DIALOG (chooser)) == GTK_RESPONSE_ACCEPT) {
+    filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (chooser));
+    g_print ("open %s\n", filename);
+    g_free (filename);
+  }
+  gtk_widget_hide (chooser);
+}
+
 int
 main (int argc, char *argv[])
 {
   GtkBuilder *builder;
-  GtkWidget *window;
+  GtkWidget *window, *open_item, *chooser, *article;
 
   utt_set_locale ();
 
@@ -29,7 +43,13 @@ main (int argc, char *argv[])
     g_error ("Can't get %s from %s", "window1", UIFILE);
   }
 
-  
+  chooser = GTK_WIDGET (gtk_builder_get_object (builder, "filechooserdialog1"));
+  article = GTK_WIDGET (gtk_builder_get_object (builder, "article1"));
+  gtk_widget_set_can_default (article, TRUE);
+
+  open_item = GTK_WIDGET (gtk_builder_get_object (builder, "imagemenuitem2"));
+  g_signal_connect (open_item, "activate", G_CALLBACK (on_open_item_activate), chooser);
+
 
   gtk_builder_connect_signals (builder, NULL);
   g_object_unref (G_OBJECT (builder));
