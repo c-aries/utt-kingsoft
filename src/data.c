4ad2083115cfeac502f49b0c4da511a993cf63e8
c-aries <babyaries2@gmail.com>
2012-04-05 00:10:45 +0800

    clean comments

--- src/data.c
+++ src/data.c
@@ -55,33 +55,3 @@ data_deinit()
   }
   g_hash_table_destroy (key_ht);
 }
-
-/* struct icon * */
-/* load_icons () */
-/* { */
-/*   gchar *path; */
-/*   gint i; */
-
-/*   if (icon[0].pix) { */
-/*     return NULL; */
-/*   } */
-
-/*   for (i = 0; i < G_N_ELEMENTS (icon); i++) { */
-/*     path = g_strjoin ("", MEDIADIR, icon[i].filename, NULL); */
-/*     if (!g_file_test (path, G_FILE_TEST_EXISTS)) { */
-/*       g_error ("%s doesn't exists", path); */
-/*     } */
-/*     icon[i].pix = gdk_pixbuf_new_from_file (path, NULL); */
-/*     g_free (path); */
-/*     icon[i].w = gdk_pixbuf_get_width (icon[i].pix); */
-/*     icon[i].h = gdk_pixbuf_get_height (icon[i].pix); */
-/*   } */
-
-/*   return icon; */
-/* } */
-
-/* struct key * */
-/* load_keys () */
-/* { */
-/*   return key; */
-/* } */
