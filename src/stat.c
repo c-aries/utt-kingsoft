a5fcf570a39760b226396c41d4af776b36f892f5
c-aries <babyaries2@gmail.com>
2012-04-17 00:23:06 +0800

    calc statistics result

--- src/stat.c
+++ src/stat.c
@@ -25,3 +25,55 @@ stat_destroy (struct _stat **stat)
   g_free (*stat);
   *stat = NULL;
 }
+
+/* per minute */
+gdouble
+stat_pass_speed (struct _stat *stat)
+{
+  gdouble ret;
+
+  if (stat->elapse == 0) {
+    return 0;
+  }
+  ret = stat->pass * 60.0 / stat->elapse;
+  return ret;
+}
+
+/* per minute */
+gdouble
+stat_correct_speed (struct _stat *stat)
+{
+  gdouble ret;
+
+  if (stat->elapse == 0) {
+    return 0;
+  }
+  ret = stat->right * 60.0 / stat->elapse;
+  return ret;
+}
+
+/* per minute */
+gdouble
+stat_finish (struct _stat *stat)
+{
+  gdouble ret;
+
+  if (stat->total == 0) {
+    return 0;
+  }
+  ret = stat->pass * 100.0 / stat->total;
+  return ret;
+}
+
+/* per minute */
+gdouble
+stat_correct (struct _stat *stat)
+{
+  gdouble ret;
+
+  if (stat->total == 0) {
+    return 0;
+  }
+  ret = stat->right * 100.0 / stat->sum;
+  return ret;
+}
--- src/stat.h
+++ src/stat.h
@@ -6,6 +6,7 @@
 
 struct _stat {		     /* statistics system */
   guint right;			/* right characaters */
+  guint pass;			/* from 0 to total, when it equals total the class finish */
   guint sum;			/* sum of characters you have input */
   guint total;			/* totoal of characters the class have */
   guint elapse;			/* time elapse */
@@ -14,5 +15,9 @@ struct _stat {		     /* statistics system */
 void stat_init (struct _stat *stat);
 struct _stat *stat_new ();
 void stat_destroy (struct _stat **stat);
+gdouble stat_pass_speed (struct _stat *stat);
+gdouble stat_correct_speed (struct _stat *stat);
+gdouble stat_finish (struct _stat *stat);
+gdouble stat_correct (struct _stat *stat);
 
 #endif
